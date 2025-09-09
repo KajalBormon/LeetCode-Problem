13e178d1f1b8b2f3be1bf2a1d09c13c7ca9f10e1
KajalBormon <kajalbormon40@gmail.com>
2025-09-09 22:24:04 +0600

    Sort Colors

---
+++ Sorting/75. Sort Colors.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        n = len(nums)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        return nums
+
+
+nums = [2,0,2,1,1,0]
+sol = Solution()
+result = sol.sortColors(nums)
+print(result)
